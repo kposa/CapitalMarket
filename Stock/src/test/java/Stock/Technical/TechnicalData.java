b06d6c5f9d15c5cd680bc71bd140d7dd7a43d48a
Kumar <kumara.1559@gmail.com>
2017-12-27 00:41:28 +0530

    Update

--- Stock/src/test/java/Stock/Technical/TechnicalData.java
+++ Stock/src/test/java/Stock/Technical/TechnicalData.java
@@ -19,7 +19,12 @@ public class TechnicalData {
 	private List<Double> MACD= new ArrayList<Double>();
 	private List<Double> Signal= new ArrayList<Double>();
 	private List<Double> Histogram= new ArrayList<Double>();
-	
+	private List<Double> MiddleBand= new ArrayList<Double>();
+	private List<Double> StandardDeviation= new ArrayList<Double>();
+	private List<Double> UpperBand= new ArrayList<Double>();
+	private List<Double> LowerBand= new ArrayList<Double>();
+	private List<Double> Bandwidth= new ArrayList<Double>();
+
 	public void calculateTechnicalData(List<TradingData> historicalData)
 	{
 		this.change.add(0.0);
@@ -33,42 +38,47 @@ public class TechnicalData {
 		{
 			this.avLoss.add(0.0);
 		}
-		
+
 		for(int iteration=166;iteration>=153;iteration--)
 		{
 			this.rs.add(0.0);
 		}
-		
+
 		for(int iteration=166;iteration>=153;iteration--)
 		{
 			this.rsi.add(0.0);
 		}
-		
+
 		for(int iteration=166;iteration>=156;iteration--)
 		{
 			this.EMA12Day.add(0.0);
 		}
-		
+
 		for(int iteration=166;iteration>=142;iteration--)
 		{
 			this.EMA26Day.add(0.0);
 		}
-		
+
 		for(int iteration=166;iteration>=142;iteration--)
 		{
 			this.MACD.add(0.0);
 		}
-		
+
 		for(int iteration=166;iteration>=134;iteration--)
 		{
 			this.Signal.add(0.0);
 		}
-		
+
 		for(int iteration=166;iteration>=134;iteration--)
 		{
 			this.Histogram.add(0.0);
 		}
-		
+
+		for(int iteration=166;iteration>=147;iteration--)
+		{
+			this.MiddleBand.add(0.0);
+		}
+
 		for(int i=166;i>=1;i--)
 		{
 			this.change.add(historicalData.get(i-1).getClose()-historicalData.get(i).getClose());
@@ -85,7 +95,7 @@ public class TechnicalData {
 				this.gain.add(0.0);
 			}
 		}
-		
+
 		for(int i=1;i<=166;i++)
 		{
 			if(this.change.get(i)<0)
@@ -104,31 +114,31 @@ public class TechnicalData {
 			gainVal+=this.gain.get(i);
 		}
 		this.avGain.add(gainVal/14);
-		
+
 		for(int i=15;i<=this.gain.size()-1;i++)
 		{
 			double val = ((this.avGain.get(i-1)*13)+this.gain.get(i))/14;
 			this.avGain.add(val);
 		}
-		
+
 		double lossVal = 0.0;
 		for(int i=1;i<=14;i++)
 		{
 			lossVal+=this.loss.get(i);
 		}
 		this.avLoss.add(lossVal/14);
-		
+
 		for(int i=15;i<=this.loss.size()-1;i++)
 		{
 			double val = ((this.avLoss.get(i-1)*13)+this.loss.get(i))/14;
 			this.avLoss.add(val);
 		}
-		
+
 		for(int i=14;i<=this.avGain.size()-1;i++)
 		{
 			this.rs.add(this.avGain.get(i)/this.avLoss.get(i));
 		}
-		
+
 		for(int i=14;i<=this.rs.size()-1;i++)
 		{
 			double val = 0.0;
@@ -142,14 +152,14 @@ public class TechnicalData {
 			}
 			this.rsi.add(val);
 		}
-		
+
 		double ema12Days = 0.0;
 		for(int i=166;i>=155;i--)
 		{
 			ema12Days+=historicalData.get(i).getClose();
 		}
 		this.EMA12Day.add(ema12Days/12);
-		
+
 		for(int i=154;i>=0;i--)
 		{
 			double a1 = (double)2/13;
@@ -157,14 +167,14 @@ public class TechnicalData {
 			double val = (historicalData.get(i).getClose()*a1)+(EMA12Day.get(165-i)*a2);
 			this.EMA12Day.add(val);
 		}
-		
+
 		double ema26Days = 0.0;
 		for(int i=166;i>=141;i--)
 		{
 			ema26Days+=historicalData.get(i).getClose();
 		}
 		this.EMA26Day.add(ema26Days/26);
-		
+
 		for(int i=140;i>=0;i--)
 		{
 			double a1 = (double)2/27;
@@ -172,19 +182,19 @@ public class TechnicalData {
 			double val = (historicalData.get(i).getClose()*a1)+(EMA26Day.get(165-i)*a2);
 			this.EMA26Day.add(val);
 		}
-		
+
 		for(int i=25;i<=EMA12Day.size()-1;i++)
 		{
 			this.MACD.add(this.EMA12Day.get(i)-this.EMA26Day.get(i));
 		}
-		
+
 		double macdValue = 0.0;
 		for(int i=25;i<=33;i++)
 		{
 			macdValue+=this.MACD.get(i);
 		}
 		this.Signal.add(macdValue/9);
-		
+
 		for(int i=34;i<=MACD.size()-1;i++)
 		{
 			double a1 = (double)2/10;
@@ -192,31 +202,40 @@ public class TechnicalData {
 			double val = (this.MACD.get(i)*a1)+(this.Signal.get(i-1)*a2);
 			this.Signal.add(val);
 		}
-		
+
 		for(int i=33;i<=Signal.size()-1;i++)
 		{
 			this.Histogram.add(this.MACD.get(i)-this.Signal.get(i));
 		}
+		for(int i=147;i>=1;i--)
+		{
+			double middleBandValue = 0.0;
+			for(int j=i+19;j>=i;j--)
+			{
+				middleBandValue+=historicalData.get(j).getClose();
+			}
+			this.MiddleBand.add(middleBandValue);
+		}
 	}
-	
+
 	public List<Double> getRSI()
 	{
 		return this.rsi;
 	}
-	
+
 	public List<Double> getMACD()
 	{
 		return this.MACD;
 	}
-	
+
 	public List<Double> getSignal()
 	{
 		return this.Signal;
 	}
-	
+
 	public List<Double> getHistogram()
 	{
 		return this.Histogram;
 	}
-	
+
 }
